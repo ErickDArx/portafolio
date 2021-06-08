931825ccba1ae85351309202569434914121e312
ErickDArx <alexalonso2706@gmail.com>
2021-06-08 16:11:01 -0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Primary Meta Tags -->
+    <title>Erick Matamoros Fernández</title>
+    <meta name="title" content="Erick Matamoros Fernández">
+    <meta name="description"
+        content="Ingeniero en sistemas dedicado a desarrollar soluciones web y diseño de interfaces de usuario aplicando tecnologías de mayor auge.   ">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://metatags.io/">
+    <meta property="og:title" content="Erick Matamoros Fernández">
+    <meta property="og:description"
+        content="Ingeniero en sistemas dedicado a desarrollar soluciones web y diseño de interfaces de usuario aplicando tecnologías de mayor auge.   ">
+    <meta property="og:image"
+        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://metatags.io/">
+    <meta property="twitter:title" content="Erick Matamoros Fernández">
+    <meta property="twitter:description"
+        content="Ingeniero en sistemas dedicado a desarrollar soluciones web y diseño de interfaces de usuario aplicando tecnologías de mayor auge.   ">
+    <meta property="twitter:image"
+        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+</head>
+
+<body class="">
+</body>
+
+</html>
