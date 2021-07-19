8e085164df7ae4a28ef16b7f34374469a24f613e
ErickDArx <alexalonso2706@gmail.com>
2021-07-19 00:04:32 -0600

    SEO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,41 +31,54 @@
 </head>
 
 <body>
-    <main class="border flex flex-col justify-between absolute inset-0 bottom-0 top-0 px-4">
-        <ul class="flex justify-between items-center py-4">
-            <li class="w-24">
-                <img src="https://github.com/ErickDArx/ErickDArx/raw/main/img/erickmatamoros.png?raw=true"
-                    alt="Logo Erick Matamoros Fernández" />
-            </li>
-            <li class="">
-                <a href="" class="text-indigo-700">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16m-7 6h7" />
-                    </svg>
-                </a>
-            </li>
-            <!-- <ul class="flex gap-6 font-semibold">
-            <li>Sobre mi</li>
-            <li>Mis proyectos</li>
-            <li>Mis habilidades</li>
-          </ul> -->
-        </ul>
-        <div class="border">
-            <div class="text-4xl font-extrabold">
-                <h1>Erick Matamoros Fernandez</h1>
-            </div>
+<main class="border flex flex-col justify-between relative inset-0 bottom-0 top-0 px-4">
+  <div class="relative z-50">
+    <div class="fixed top-0 right-0 left-0 px-4 py-2 backdrop-blur-sm">
+      <ul class="transition-all px-2 flex justify-between items-center py-4 font-thin text-gray-900 relative z-50 gap-2 text-sm sm:text-base lg:text-lg">
+        <li class="transition-all px-2 font-medium text-green-900">Sobre mi</li>
+        <li class="transition-all px-2 py-2 hover:bg-green-900 hover:rounded hover:py-2 hover:text-gray-50"><a href="" title="Mis Proyectos">Mis proyectos</a></li>
+        <li>Mis habilidades</li>
+      </ul>
+      <div class="absolute inset-0 bg-gray-200 opacity-50"></div>
+    </div>
+  </div>
 
-            <div>
-                <h2>Ingeniero en sistemas</h2>
-            </div>
-        </div>
+  <div class="h-screen flex flex-col justify-between">
+    <div></div>
 
-        <footer>
+    <!-- <div class="flex justify-center mt-10">
+      <div class="bg-blue-600 inset-0 px-10 py-10 transform skew-y-12"></div>
+      <div class="bg-blue-400 inset-0 px-10 py-10 transform -skew-y-12"></div>
+    </div> -->
 
-        </footer>
-    </main>
+    <div class="mt-5"></div>
+    <!-- <div class="grid grid-cols-3 gap-4 justify-center items-center">
+      <div class="bg-indigo-900  px-4 py-2 rounded flex justify-center items-center text-gray-50 text-sm">PHP</div>
+      <div class="bg-purple-900 px-4 py-2 flex rounded justify-center items-center text-gray-50 text-sm">ASP.NET</div>
+      <div class="bg-red-700 px-4 py-2 flex rounded justify-center items-center text-gray-50 text-sm">Java</div>
+      <div class="col-span-2 bg-yellow-400 px-4 py-2 flex rounded justify-center items-center text-gray-900 text-sm">JavaScript</div>
+      <div class="bg-yellow-600 px-4 py-2 flex rounded justify-center items-center text-gray-50 text-sm">HTML</div>
+      <div class="bg-yellow-300">6</div>
+      <div class="col-span-2 bg-sky-600">7</div>
+    </div> -->
+    <div>
+      <div class="text-4xl font-extrabold">
+        <h1>Erick Matamoros Fernandez</h1>
+      </div>
+
+      <div>
+        <h2>Ingeniero en sistemas</h2>
+      </div>
+    </div>
+
+      <div class=""></div>
+  </div>
+
+  <div class="h-screen"></div>
+
+  <footer></footer>
+</main>
+
 </body>
 
 </html>
