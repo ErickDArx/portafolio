baf713934d72709d00102afcdc37955dee935d17
ErickDArx <alexalonso2706@gmail.com>
2021-07-30 21:43:37 -0600

    Reinicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,125 +2,116 @@
 <html lang="es">
 
 <head>
-    <meta name="google-site-verification" content="54nOkmbODnNd8AJopsYw-N_7FmcdWa0LqAknVdN6iAY" />
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <!-- Primary Meta Tags -->
-    <title>Erick Matamoros Fernández</title>
-    <meta name="title" content="Erick Matamoros Fernández">
-    <meta name="description"
-        content="Ingeniero en sistemas dedicado a desarrollar soluciones web y diseño de interfaces de usuario aplicando tecnologías de mayor auge.   ">
+  <meta name="google-site-verification" content="54nOkmbODnNd8AJopsYw-N_7FmcdWa0LqAknVdN6iAY" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+  <!-- Primary Meta Tags -->
+  <title>Erick Matamoros Fernández</title>
+  <meta name="title" content="Erick Matamoros Fernández">
+  <meta name="description"
+    content="Ingeniero en sistemas dedicado a desarrollar soluciones web y diseño de interfaces de usuario aplicando tecnologías de mayor auge.   ">
 
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://erickmatamoros.herokuapp.com/">
-    <meta property="og:title" content="Erick Matamoros Fernández">
-    <meta property="og:description"
-        content="Ingeniero en sistemas dedicado a desarrollar soluciones web y diseño de interfaces de usuario aplicando tecnologías de mayor auge.   ">
-    <meta property="og:image" content="{{ asset('css/img/Erick Matamoros Fernández.svg') }}">
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://erickmatamoros.herokuapp.com/">
+  <meta property="og:title" content="Erick Matamoros Fernández">
+  <meta property="og:description"
+    content="Ingeniero en sistemas dedicado a desarrollar soluciones web y diseño de interfaces de usuario aplicando tecnologías de mayor auge.   ">
+  <meta property="og:image" content="{{ asset('css/img/Erick Matamoros Fernández.svg') }}">
 
-    <!-- Twitter -->
-    <meta property="twitter:card" content="Ingeniero en sistemas">
-    <meta property="twitter:url" content="https://erickmatamoros.herokuapp.com/">
-    <meta property="twitter:title" conwitter:descriptiontent="Erick Matamoros Fernández">
-    <meta property="t"
-        content="Ingeniero en sistemas dedicado a desarrollar soluciones web y diseño de interfaces de usuario aplicando tecnologías de mayor auge.   ">
-    <meta property="twitter:image" content="{{ asset('css/img/Erick Matamoros Fernández.svg') }}">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <!-- Twitter -->
+  <meta property="twitter:card" content="Ingeniero en sistemas">
+  <meta property="twitter:url" content="https://erickmatamoros.herokuapp.com/">
+  <meta property="twitter:title" conwitter:descriptiontent="Erick Matamoros Fernández">
+  <meta property="t"
+    content="Ingeniero en sistemas dedicado a desarrollar soluciones web y diseño de interfaces de usuario aplicando tecnologías de mayor auge.   ">
+  <meta property="twitter:image" content="{{ asset('css/img/Erick Matamoros Fernández.svg') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,200&display=swap" rel="stylesheet" />
+
 </head>
 
-<body class="flex flex-col justify-between relative bg-gradient-to-b from-green-600 to-blue-900">
-  <div class="relative z-50">
-    <div class="fixed top-0 right-0 left-0 backdrop-blur-sm">
-      <div class="absolute inset-0 opacity-10"></div>
-      <ul class="transition-all px-4 flex justify-between items-center py-4 font-bold text-gray-900 relative z-50 gap-2 text-base uppercase">
-        <li class="transition-all px-2 font-extrabold">Sobre mi</li>
-        <li class="text-gray-600">
-          <div class="flex gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <div>
-              <p>ES</p>
-            </div>
-          </div>
-        </li>
-        <!-- <li class="transition-all px-2 py-2 hover:bg-green-900 hover:rounded hover:py-2 hover:text-gray-50"><a href="" title="Mis Proyectos">Proyectos</a></li> -->
-        <li class="p-2 text-gray-200 rounded-full bg-gray-900">
+<body>
+  <header class="h-screen relative flex flex-col">
+    <div class="py-9 relative sm:py-7">
+      <!-- <div class="absolute h-full w-full inset-0 bg-center bg-cover bg-no-repeat" style="background-image: url(https://images.pexels.com/photos/952670/pexels-photo-952670.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500)"></div> -->
+    </div>
+  
+    <nav class="z-50 fixed top-0 right-0 left-0 text-gray-900 backdrop-blur-sm shadow-lg flex px-10 py-4 flex-row justify-between items-center font-Montserrat transition duration-75">
+      <div class="absolute inset-0 bg-gray-200 opacity-30"></div>
+  
+      <div class="z-50"><a href="" title="ERICK" class="hover:underline">ERICK</a></div>
+  
+      <ul class="z-50 hidden sm:flex flex-row justify-between gap-5 disabled:hidden invisible sm:visible sm:text-xs lg:text-xs">
+        <li>SOBRE MI</li>
+        <li>PROYECTOS</li>
+        <li>CONOCIMIENTOS</li>
+        <li>CONTACTO</li>
+      </ul>
+  
+      <div class="z-50 flex justify-center items-center gap-5 visible sm:hidden">
+        <div class="flex gap-2 justify-center items-center hover:bg-gray-900 p-2 rounded-sm hover:text-gray-200">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          <p>ES</p>
+        </div>
+        <div class="visible sm:hidden p-1 transition-all hover:bg-gray-900 hover:text-gray-100 rounded-sm">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
           </svg>
-        </li>
-      </ul>
-
-      <div class="absolute inset-0 bg-gray-200 opacity-50 shadow-xl"></div>
-      <div class="transform translate-x-1 translate-y-1 absolute inset-0 bg-gray-200 opacity-30"></div>
-    </div>
-  </div>
-  <div class="h-screen flex flex-col justify-between relative">
-    <!-- <div class="grid grid-rows-3 grid-cols-3 gap-0 w-6/12 absolute right-0 top-0">
-      <div class="bg-fixed row-span-3 bg-no-repeat bg-center bg-cover" style="background-image: url(https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500)"></div>
-      <div class="bg-fixed col-span-2 shadow-lg py-24 min-w-full h-2/6 w-2/4 bg-cover bg-center bg-no-repeat" style="background-image: url(https://images.pexels.com/photos/34600/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)"></div>
-      <div class="bg-fixed row-span-2 col-span-2 shadow-lg py-24 min-w-full min-h-full h-2/6 w-2/4 bg-cover bg-center bg-no-repeat" style="background-image: url(https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500)"></div>
-    </div> -->
-    <!-- <div class="grid grid-cols-3 gap-4 justify-center items-center">
-      <div class="bg-indigo-900  px-4 py-2 rounded flex justify-center items-center text-gray-50 text-sm">PHP</div>
-      <div class="bg-purple-900 px-4 py-2 flex rounded justify-center items-center text-gray-50 text-sm">ASP.NET</div>
-      <div class="bg-red-700 px-4 py-2 flex rounded justify-center items-center text-gray-50 text-sm">Java</div>
-      <div class="col-span-2 bg-yellow-400 px-4 py-2 flex rounded justify-center items-center text-gray-900 text-sm">JavaScript</div>
-      <div class="bg-yellow-600 px-4 py-2 flex rounded justify-center items-center text-gray-50 text-sm">HTML</div>
-      <div class="bg-yellow-300">6</div>
-      <div class="col-span-2 bg-sky-600">7</div>
-    </div> -->
-
-    <div class="absolute top-20 left-0 right-0 z-10">
-      <div class="text-gray-200 p-5 grid justify-center items-center relative backdrop-blur-sm">
-        <div class="absolute inset-0 rounded-xl"></div>
-        <div class="relative">
-          <div class="text-4xl font-extrabold">
-            <h1>Erick Matamoros Fernández</h1>
-          </div>
-          <div>
-            <h2 class="font-medium">Ingeniero en sistemas</h2>
-          </div>
-        </div>
-        <div class="absolute -bottom-24 right-0 left-0 px-5">
-          <p class="text-left text-gray-200 font-normal">Aprendiz de tecnologías e ingeniería de interfaz más destacadas y utilizadas para ambientes de desarrollo web.</p>
-          <div class="absolute mt-4 right-0 left-0 px-5">
-            <ol>
-              <li>
-                <a href="" class="flex">
-                  <div class="flex gap-2 border-2 border-gray-900 p-2 text-gray-900 font-medium rounded">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24">
-                      <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                    </svg>
-                    <p>GitHub</p>
-                  </div>
-                </a>
-              </li>
-            </ol>
-          </div>
         </div>
       </div>
-    </div>
-
-    <div class="flex justify-center items-center py-4 text-gray-200 absolute bottom-0 left-0 right-0 bg-fixed" style="background-image: url()">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 bg-fixed" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    </nav>
+  
+    <main class="px-10 py-4 font-Montserrat relative select-none">
+      <div class="absolute inset-0 bg-gray-900"></div>
+      <div class="text-gray-200 relative grid grid-cols-3 grid-rows-1">
+        <div class="col-span-2">
+          <p>ANALIZAR</p>
+          <p>DESARROLLAR</p>
+          <p>IMPLEMENTAR</p>
+        </div>
+        <div class="bg-contain bg-no-repeat" style="background-image: url(https://raw.githubusercontent.com/ErickDArx/portafolio/5d0746acc0caa9f8744ddc089b7ca60f6b777245/5143537_adobespark.svg)">
+  
+        </div>
+      </div>
+    </main>
+  
+    <main class="px-10 py-4 font-Poppins text-xl leading-8">
+      <h1>
+        <strong class="font-Montserrat block tracking-wide"> HOLA !</strong> Soy Erick Matamoros <br />
+        Ingeniero en sistemas de computaci&oacuten y desarrollador web
+      </h1>
+  
+      <p class="text-base mt-2 italic mb-4 underline">Apasionado por el conocimiento y la web</p>
+  
+      <a href="#" class="text-sm p-2 bg-gray-900 rounded-sm text-gray-200">Comenzar recorrido</a>
+    </main>
+  
+    <!-- <div class="absolute bottom-0 right-0 left-0 flex justify-center items-center py-4 text-gray-900 animate-bounce">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
       </svg>
-    </div>
-
-    <!-- <div class="bg-gray-900 px-4 py-6 z-10 fixed bottom-0 right-0 left-0">
-      <div class="grid grid-cols-1 grid-rows-1 justify-center items-center text-gray-200">
-        <p>Continuar</p>
-      </div>
     </div> -->
-  </div>
-
-  <div class="h-screen bg-gradient-to-t"></div>
+  </header>
   
-  <footer></footer>
+  <section class="h-screen relative flex justify-center items-center bg-gray-200 z-10">
+    <div class="font-Montserrat">
+      <h2>EN CONSTRUCCION</h2>
+    </div>
+  </section>
+  
+  <section class="h-screen sticky top-0 flex justify-center items-center bg-gray-200">
+    <div class="font-Montserrat">
+      <h2>EN CONSTRUCCION</h2>
+    </div>
+  </section>
 </body>
 
 </html>
