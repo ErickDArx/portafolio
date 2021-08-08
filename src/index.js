c07fcf1941aad55415b7688d64d4f727e47018d4
ErickDArx <alexalonso2706@gmail.com>
2021-08-08 14:07:15 -0600

    Test deploy in Heroku

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 // Llamar a express y crear el servidor
 const express = require('express');
+const app = express();
+const routes = require('./routes/web');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/web')
+
 // Atraer las variables de entorno
 require('dotenv').config();
 
+// Conexion base de datos
 const mongoose = require('mongoose');
-const app = express();
-
 mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.DB_MONGO ,  { useNewUrlParser: true,useUnifiedTopology: true })
-.then(db => console.log('Todo bien'))
-.catch(err =>console.log(err));
+mongoose.connect(process.env.DB_MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(db => console.log('La conexion a la base de datos es correcta'))
+    .catch(err => console.log(err));
+
 const path = require('path');
+
+// Conexion al servidor local
 const port = process.env.PORT || 8080; // Si está definido en el entorno, usarlo. Si no, el 3000
+
 // Configuraciones
 app.set('port', port);
 app.set('views', path.join(__dirname, 'views/'));
@@ -21,16 +27,16 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 // Middlewares || Procesar acciones en las URL's
-app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Routes 
-app.use(require('./routes/web'));
 
 // static files || Enviar archivos al Frontend
 app.use(express.static(__dirname + '/public'));
 
+// Routes 
+app.use('/', routes);
+
 // Escuchando al servidor
 app.listen(app.get('port'), () => {
-    console.log('Server on port', port);
+    console.log('El Servidor se encuentra funcionado en este momento', port);
 });
